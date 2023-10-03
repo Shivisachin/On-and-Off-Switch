3a2d4402b4693bc847916fbb3247b4ed3b89e645
kavitagashni <kavitagashni@gmail.com>
2023-10-03 17:32:54 +0530

    blub button

--- turn.js
+++ turn.js
@@ -1,15 +1,20 @@
 const button = document.querySelector("#btn")
-var text = 0;
+var click = 0;
 button.addEventListener('click', changeToButton);
 function changeToButton() {
-    if (text === 0) {
+    if (click === 0) {
         button.textContent = "Turn on"
+        const img = document.querySelector("#image")
+        img.setAttribute("src", "bulb-on.png")
         button.style.backgroundColor = "green";
-        text = 1
+        click = 1
     }
     else {
         button.textContent = "Turn off"
         button.style.backgroundColor = "white";
-        text = 0
+        const img = document.querySelector("#image")
+        img.setAttribute("src", "bulb-off.png")
+        click = 0
     }
 }
+
